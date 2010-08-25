ba54b8282627d6130c9b43ae3e555db50c3744a6
David Herman <dherman@David-Hermans-MacBook-Pro.local>
2010-08-24 17:10:48 -0700

    - `dr.js -n` to run static site without JSON server (useful for testing UI error behavior) - lots of UI changes:   * twitter-like notification bar for errors   * cosmetic changes to the tooltip bubbles in index.html   * ajaxy progress animation while waiting for server   * fixed a variable shadowing bug (status) that was causing tooltip bubbles to reload every time   * try.html puts results in a formatted div instead of a textarea   * try.html results are pretty-fied instead of just given as raw JSON

---
+++ html/doctorjs/parse-type.js
@@ -0,0 +1,91 @@
+// quick-n-dirty parser of Dimitris's original type format
+function parseType(src) {
+    function match(str) {
+        var n = str.length;
+        if (src.length && src.substring(0, n) === str) {
+            src = src.substring(n).trim();
+            return true;
+        }
+        return false;
+    }
+
+    function mustMatch(str, msg) {
+        if (!match(str))
+            throw new Error(msg + " (" + src + ")");
+    }
+
+    function Type() {
+        var root;
+        if (match("any")) {
+            root = "any";
+        } else if (match("string")) {
+            root = "string";
+        } else if (match("number")) {
+            root = "number";
+        } else if (match("Array")) {
+            if (match("[")) {
+                root = { kind: "array", elementType: Type() };
+                mustMatch("]", "invalid array type");
+            } else {
+                root = { kind: "array", elementType: "any" };
+            }
+        } else if (src.length && src[0] === "<") {
+            root = UnionType();
+        } else {
+            var re = /^([a-zA-Z_$][a-zA-Z_$0-9]*)/;
+            var result = re.exec(src);
+            if (!result)
+                throw new Error("unrecognized type: " + src);
+            root = result[0];
+            src = src.substring(root.length).trim();
+        }
+
+        if (match("function")) {
+            mustMatch("(", "invalid function type");
+            var args = [];
+            if (!match(")")) {
+                do {
+                    args.push(Type());
+                } while (match(","));
+                mustMatch(")", "invalid function type");
+            }
+            return { kind: "function", returnType: root, argTypes: args };
+        }
+
+        return root;
+    }
+
+    function UnionType() {
+        mustMatch("<", "invalid union type");
+        var variants = [];
+        if (!match(">")) {
+            do {
+                variants.push(Type());
+            } while (match("|"));
+            mustMatch(">", "invalid union type");
+        }
+        return { kind: "union", variants: variants };
+    }
+
+    return Type();
+}
+
+// formatType : JSON type -> HTML source
+// render the JSON type as pretty HTML
+function formatType(type) {
+    if (typeof type === "string")
+        return type;
+
+    if (type.kind === "function") {
+        return "function(" +
+               type.argTypes.map(formatType).join(", ") +
+               ") &rarr; " +
+               formatType(type.returnType);
+    } else if (type.kind === "array") {
+        return "Array[" + formatType(type.elementType) + "]";
+    } else if (type.kind === "union") {
+        return "&lt;" + type.variants.map(formatType).join(" | ") + "&gt;";
+    }
+
+    throw new Error("unrecognized type");
+}
